9343b07c4e2809104be79052124069024da623f7
Diego Neri <diego@diegoneri.com>
2023-09-29 22:28:42 -0300

    Finaliza programa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int mes;
+Console.WriteLine("--- Mês por extenso ---");
+Console.Write("Digite o número do mês...: ");
+
+mes = Convert.ToInt32(Console.ReadLine());
+
+switch (mes)
+{
+    case 1:
+        Console.WriteLine("Janeiro");
+        break;
+    case 2:
+        Console.WriteLine("Fevereiro");
+        break;
+    case 3:
+        Console.WriteLine("Março");
+        break;
+    case 4:
+        Console.WriteLine("Abril");
+        break;
+    case 5:
+        Console.WriteLine("Maio");
+        break;
+    case 6:
+        Console.WriteLine("Junho");
+        break;
+    case 7:
+        Console.WriteLine("Julho");
+        break;
+    case 8:
+        Console.WriteLine("Agosto");
+        break;
+    case 9:
+        Console.WriteLine("Setembro");
+        break;
+    case 10:
+        Console.WriteLine("Outubro");
+        break;
+    case 11:
+        Console.WriteLine("Novembro");
+        break;
+    case 12:
+        Console.WriteLine("Dezembro");
+        break;
+    default:
+        Console.WriteLine("Mês inválido");
+        break;
+}
